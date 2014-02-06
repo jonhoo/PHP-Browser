63555a9e953bfec4b54701dcfeea32853c1352d2
Jon Gjengset <jon@thesquareplanet.com>
2014-02-06 00:30:30 +0000

    Make example work again

--- examples/browser.example.php
+++ examples/browser.example.php
@@ -23,10 +23,9 @@ $b -> navigate ( 'http://en.wikipedia.org/wiki/Main_Page' );
  * Search for php
  */
 $b -> submitForm (
-      $b  -> getForm ( "//form[@id='searchform']" ) 
-	      -> setAttributeByName ( 'search', 'php' ),
+      $b  -> getForm ( "//form[@id='searchform']" )
+          -> setAttributeByName ( 'search', 'php' ),
       'fulltext'
   )
-  -> click ( "//a[@title='PHP']" ) // Click the PHP search result
   -> click ( "PHP Reference Manual" ); // Click the link to the ref
 echo $b -> getSource(); // Output the source
